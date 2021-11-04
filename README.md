84be7268ca8d31fb6b76675e0eca54eaa25c035b
Kevin Buffardi <kbuffardi@csuchico.edu>
2021-11-03 20:43:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scholar Website
+
+Academic website to represent teaching, research, and service activities for Professor Dr. Kevin Buffardi. The webpage is published on [GitHub Pages](https://kbuffardi.github.io/ScholarWebsite/)
---
+++ scholar.js
@@ -0,0 +1,40 @@
+window.onload = initialize;
+
+var DATAFILE = "research.json";
+var DONE_READYSTATE = 4;
+var DONE_STATUS = 200;
+var OFFICIAL_URL = "https://kbuffardi.github.io/ScholarWebsite/";
+var OFFICIAL_HOST = "kbuffardi.github.io"
+
+var references = {};
+
+function initialize()
+{
+  //validateHost();
+  //loadExternalData();
+}
+
+function validateHost()
+{
+  if( window.location.hostname != OFFICIAL_HOST )
+  {
+    window.location.href = OFFICIAL_URL;
+  }
+}
+
+function loadExternalData()
+{
+  var json = new XMLHttpRequest();
+  json.overrideMimeType("application/json");
+  //event listener: when json file load is "done"
+  json.onreadystatechange = function() {
+    if (this.readyState == DONE_READYSTATE
+        && this.status == DONE_STATUS)
+    {
+      references=JSON.parse(this.responseText);
+      console.log(references);
+    }
+  }
+  json.open("GET", DATAFILE);
+  json.send();
+}
