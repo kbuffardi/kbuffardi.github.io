d89da878c5afc107a82ecf73f223603054798c5d
Kevin Buffardi <kbuffardi@csuchico.edu>
2023-08-12 20:32:15 -0700

    Add research active projects

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -70,8 +70,134 @@ export default function Research() {
           <div className="flex w-5/6 justify-center flex-wrap">
             <div className="p-6 m-px bg-neutral-800 border border-neutral-700 rounded-lg shadow w-full">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Active Projects</h5>
-              <p className="mb-2 text-lg font-medium tracking-tight text-gray-300">Research Areas</p>
-              <div className="flex-wrap"></div>
+              <p className="mb-2 text-lg font-medium tracking-tight text-gray-300">
+                <span className="mr-2 font-normal">Research Area:</span>
+                <span className="flex-nowrap whitespace-nowrap">
+                  <span className="mr-1 font-light text-green-400">Computer Science Education</span>
+                  <span className="inline-block whitespace-nowrap rounded-full bg-green-400 ml-1 mr-2 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                    ED
+                  </span>
+                </span>
+                <span className="flex-nowrap whitespace-nowrap">
+                  <span className="mr-1 font-light text-amber-500">Broadening Participation</span>
+                  <span className="inline-block whitespace-nowrap rounded-full bg-amber-500 ml-1 mr-2 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                    BP
+                  </span>
+                </span>
+                <span className="flex-nowrap whitespace-nowrap">
+                  <span className="mr-1 font-light text-sky-400">Adaptive Feedback</span>
+                  <span className="inline-block whitespace-nowrap rounded-full bg-sky-400 ml-1 mr-2 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                    AF
+                  </span>
+                </span>
+                <span className="flex-nowrap whitespace-nowrap">
+                  <span className="mr-1 font-light text-red-600">Software Testing</span>
+                  <span className="inline-block whitespace-nowrap rounded-full bg-red-600 ml-1 mr-2 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                    ST
+                  </span>
+                </span>
+                <span className="flex-nowrap whitespace-nowrap">
+                  <span className="mr-1 font-light text-indigo-400">Software Engineering</span>
+                  <span className="inline-block whitespace-nowrap rounded-full bg-indigo-400 ml-1 mr-2 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                    SE
+                  </span>
+                </span>
+                <span className="flex-nowrap whitespace-nowrap">
+                  <span className="mr-1 font-light text-pink-600">Usability &amp; User eXperience</span>
+                  <span className="inline-block whitespace-nowrap rounded-full bg-pink-600 ml-1 mr-2 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                    UX
+                  </span>
+                </span>
+                <span className="flex-nowrap whitespace-nowrap">
+                  <span className="mr-1 font-light text-lime-200">Entrepreneurship</span>
+                  <span className="inline-block whitespace-nowrap rounded-full bg-lime-200 ml-1 mr-2 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                    EN
+                  </span>
+                </span>
+              </p>
+              <p className="mt-4 text-lg font-bold tracking-tight">
+                Codewit.us
+                <span className="inline-block whitespace-nowrap rounded-full bg-green-400 ml-1 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                  ED
+                </span>
+                <span className="inline-block whitespace-nowrap rounded-full bg-amber-500 ml-1 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                  BP
+                </span>
+                <span className="inline-block whitespace-nowrap rounded-full bg-sky-400 ml-1 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                  AF
+                </span>
+              </p>
+              <p className="m-0 text-base font-light text-gray-300 tracking-tight">
+                <a href="https://codewit.us/" target="_blank" className="font-semibold underline hover:font-bold">
+                  Codewit.us
+                </a> 
+                is an inclusive platform for sharing unique perspectives in learning how to code. 
+                Funded by <a href="https://calearninglab.org/project/coding-community-inclusive-space-for-programming-tutorials-and-adaptive-learning/" target="_blank" className="font-semibold underline hover:font-bold">
+                  California Governor's Office of Planning and Research - Learning Lab (CELL)
+                </a> and&nbsp; 
+                <a href="https://www.csuchico.edu/coronavirus/federal-aid/index.shtml" target="_blank" className="font-semibold underline hover:font-bold">
+                  Department of Education (DOE) Higher Education Emergency Relief Fund (HEERF II)
+                </a>.
+              </p>
+
+              <p className="mt-4 text-lg font-bold tracking-tight">
+                CURE-E
+                <span className="inline-block whitespace-nowrap rounded-full bg-green-400 ml-1 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                  ED
+                </span>
+                <span className="inline-block whitespace-nowrap rounded-full bg-amber-500 ml-1 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                  BP
+                </span>
+                <span className="inline-block whitespace-nowrap rounded-full bg-lime-200 ml-1 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                  EN
+                </span>
+              </p>
+              <p className="m-0 text-base font-light text-gray-300 tracking-tight">
+                CURE-E uses a unique combination of <span className="italic">Course-based Undergraduate Research Experience</span> pedagogy with <span className="italic">entrepreneurship</span>&nbsp;
+                to teach authentic software development and research skills in undergraduate Computer Science courses.
+                Funded by <a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1953751" target="_blank" className="font-semibold underline hover:font-bold">
+                  National Science Foundation (NSF)
+                </a>.
+              </p>
+
+              <p className="mt-4 text-lg font-bold tracking-tight">
+                Testoscope
+                <span className="inline-block whitespace-nowrap rounded-full bg-indigo-400 ml-1 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                  SE
+                </span>
+                <span className="inline-block whitespace-nowrap rounded-full bg-green-400 ml-1 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                  ED
+                </span>
+                <span className="inline-block whitespace-nowrap rounded-full bg-red-600 ml-1 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                  ST
+                </span>
+                <span className="inline-block whitespace-nowrap rounded-full bg-sky-400 ml-1 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                  AF
+                </span>
+              </p>
+              <p className="m-0 text-base font-light text-gray-300 tracking-tight">
+                Innovating software testing education tools, methods, and metrics by investigating how software developers critically evaluate software with unit tests.
+              </p>
+
+              <p className="mt-4 text-lg font-bold tracking-tight">
+                Tech Startup Model
+                <span className="inline-block whitespace-nowrap rounded-full bg-indigo-400 ml-1 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                  SE
+                </span>
+                <span className="inline-block whitespace-nowrap rounded-full bg-pink-600 ml-1 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                  UX
+                </span>
+                <span className="inline-block whitespace-nowrap rounded-full bg-lime-200 ml-1 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                  EN
+                </span>
+                <span className="inline-block whitespace-nowrap rounded-full bg-green-400 ml-1 px-[0.65em] pb-[0.25em] pt-[0.35em] text-center align-baseline text-[0.75em] font-bold leading-none text-black">
+                  ED
+                </span>
+              </p>
+              <p className="m-0 text-base font-light text-gray-300 tracking-tight">
+                Empowering entrepreneurship and authentic software development skills in user-centered interdisciplinary Software Engineering collaborations
+              </p>
+
             </div>
           </div>
         </main> 
