39a6ea865233d687916d23c391c1fd100faf8f84
Kevin Buffardi <kbuffardi@csuchico.edu>
2024-08-17 01:00:43 -0700

    Update with Fall 2024 information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,9 @@ export default function Home() {
             <div className="p-6 m-0.5 bg-neutral-800 border border-neutral-700 rounded-lg shadow w-full">
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-white">Office Hours (OCNL 220)</h5>
               <div className="flex-wrap text-neutral-300">
-                <p><span className="font-bold">Summer</span><span className="font-thin">&nbsp;- no office hours</span></p>
+                <p><span className="font-bold">Fall 2024</span>></p>
+                <p><span className="font-bold">Tuesday</span><span className="font-thin">&nbsp;11am-1pm</span></p>
+                <p><span className="font-bold">Thursday</span><span className="font-thin">&nbsp;11am-1pm</span></p>
                 <p>
                   <a href="https://www.csuchico.edu/csci/faculty-office-hours/index.shtml" target='_blank'>
                     <button className="bg-neutral-800 hover:bg-neutral-700 text-gray-100 font-semibold py-2 px-4 border border-neutral-400 rounded shadow inline-flex items-center">
